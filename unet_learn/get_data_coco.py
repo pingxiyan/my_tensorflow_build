b7751f506b7f547f464d44fc4353fdefe2986f7d
xipingya <xiping.yan@intel.com>
2019-06-19 14:40:06 +0800

    init train. pass. but I don't know if get a good result.
    Signed-off-by: xipingya <xiping.yan@intel.com>

--- unet_learn/get_data_coco.py
+++ unet_learn/get_data_coco.py
@@ -5,6 +5,7 @@ import pycocotools.mask as mask
 from pycocotools.coco import COCO
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 # brief Parsing coco dataset
 #	http://cocodataset.org/#download
@@ -12,7 +13,7 @@ import numpy as np
 # dataDir: root path
 # dataType: "train2014", "val2014", "test2014"
 # return data, mask
-def get_data_coco(dataDir, dataType="train2014"):
+def get_data_coco(dataDir, saveDir, dataType="train2014"):
 	annType = ['segm', 'bbox', 'keypoints']
 	annType = annType[0]  # specify type here
 	prefix = 'person_keypoints' if annType == 'keypoints' else 'instances'
@@ -37,8 +38,9 @@ def get_data_coco(dataDir, dataType="train2014"):
 
 	img = coco.loadImgs(imgIds[np.random.randint(0, len(imgIds))])[0]
 
-	print("Image total = ", len(imgIds))
-	for idx in range(len(imgIds)):
+	total_num = len(imgIds)
+	print("Image total = ", total_num)
+	for idx in range(total_num):
 		img = coco.loadImgs(imgIds[idx])[0]
 
 		img_fn = dataDir + "/" + dataType + "/" + img["file_name"]
@@ -60,15 +62,35 @@ def get_data_coco(dataDir, dataType="train2014"):
 			else:
 				mask_img = mask_img | mask
 
-		bgrmask = cv2.cvtColor(mask_img*255, cv2.COLOR_GRAY2BGR)
-		cv2.imshow("mask", bgrmask | show_img)
-		cv2.waitKey(0)
+		svFn = saveDir+"/images/"+img["file_name"]
+		svMaskFn = saveDir+"/mask/"+img["file_name"]
+		# print(svFn)
+		# print(svMaskFn)
+		cv2.imwrite(svFn, show_img)
+		cv2.imwrite(svMaskFn, mask_img*255)
+		print("\rtotal[", total_num, ",", idx, "]")
+		# exit(0)
+		# mask_img = mask_img + 1
+		# bgrmask = cv2.cvtColor(mask_img, cv2.COLOR_GRAY2BGR)
+		# mask_img = mask_img/2
+
+		# cv2.imshow("mask", mask_img*255)
+		# cv2.waitKey(0)
 	return None, None
 
 def main():
 	dataDir="/home/xiping/mydisk2/imglib/my_imglib/coco"
-	dataType = "val2014"
-	val_data, val_mask = get_data_coco(dataDir, dataType)
+	dataType = "train2014"
+	saveDir = "/home/xiping/mydisk2/imglib/my_imglib/coco/train2014_person"
+
+	if not os.path.exists(saveDir):
+		os.mkdir(saveDir)
+	if not os.path.exists(saveDir + "/images"):
+		os.mkdir(saveDir + "/images")
+	if not os.path.exists(saveDir + "/mask"):
+		os.mkdir(saveDir + "/mask")
+
+	get_data_coco(dataDir, saveDir, dataType)
 
 if __name__ == '__main__':
 	main()
--- unet_learn/train.py
+++ unet_learn/train.py
@@ -4,17 +4,61 @@ sys.path.insert(0, '../segmentation_models')
 
 import tensorflow as tf
 import numpy as np
-import cv2 as cv
+import cv2
 
 from segmentation_models import Unet
 from segmentation_models.backbones import get_preprocessing
 from segmentation_models.losses import bce_jaccard_loss
 from segmentation_models.metrics import iou_score
 
-def get_data(path, train=True):
-	return None, None
+import os
+def get_data(imgDir):
+	img_path = imgDir + "/images"
+	mask_path = imgDir + "/mask"
+	img_files = [ f for f in os.listdir(img_path) if os.path.isfile(os.path.join(img_path,f)) ]
+	# mask_files = [ f for f in os.listdir(mask_path) if os.path.isfile(os.path.join(mask_path,f)) ]
+
+	# print(len(img_files))
+	# print(img_files[0])
+	# exit(0)
+
+	train_data = []
+	mask_data = []
+	# ttt=np.array()
+	print("Start load images ...")
+	total_num = len(img_files)
+	for idx, fn in enumerate(img_files):
+		if idx > 8000:
+			break
+		print("load progress = [", total_num, ",", idx, "]")
+		img = cv2.imread(img_path + "/" + fn)
+		if img is None:
+			print("Can't imread: ", img_path + "/" + fn)
+			continue
+		mask = cv2.imread(mask_path + "/" + fn, 0)
+		if mask is None:
+			print("Can't imread: ", mask_path + "/" + fn)
+			continue
+		mask=mask.reshape(mask.shape[0],mask.shape[1],1)
+
+		img=cv2.resize(img, (224,224))/255.0
+		mask=cv2.resize(mask, (224,224)).reshape(224,224,1)/255.0
+
+		train_data.append(img)
+		mask_data.append(mask)
+
+	print("Load images finish")
+	return np.array([i for i in train_data]), np.array([i for i in mask_data])
 
 def main():
+	# train_imgDir = "/home/xiping/mydisk2/imglib/my_imglib/coco/train2014_person"
+	train_imgDir = "/coco/train2014_person"
+	train_data, mask_data = get_data(train_imgDir)
+
+	# print(train_data.shape)
+	# print(mask_data.shape)
+	# exit(0)
+
 	print("================================")
 	BACKBONE = 'mobilenetv2'
 	# define model
@@ -25,17 +69,19 @@ def main():
 
 	model.compile('Adam', loss=bce_jaccard_loss, metrics=[iou_score])
 
+	
 	print("================================")
 	print("Start train...")
 	# fit model
 	# if you use data generator use model.fit_generator(...) instead of model.fit(...)
 	# more about `fit_generator` here: https://keras.io/models/sequential/#fit_generator
-	# model.fit(
-	#     x=x_train,
-	#     y=y_train,
-	#     batch_size=16,
-	#     epochs=100,
-	#     validation_data=(x_val, y_val),
-	# )
+	model.fit(
+	    x=train_data,
+	    y=mask_data,
+	    batch_size=256,
+	    epochs=100,
+	    # validation_data=(x_val, y_val),
+	)
+
 if __name__ == '__main__':
 	main()
