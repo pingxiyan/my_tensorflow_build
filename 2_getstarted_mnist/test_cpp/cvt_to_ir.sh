46238c530f53adf3d07f6214ba3728f173e669f4
xipingya <xiping.yan@intel.com>
2019-06-26 09:20:22 +0800

    verify IE test tf pb success.
    Signed-off-by: xipingya <xiping.yan@intel.com>

--- 2_getstarted_mnist/test_cpp/cvt_to_ir.sh
+++ 2_getstarted_mnist/test_cpp/cvt_to_ir.sh
@@ -3,6 +3,7 @@
 mo="/opt/intel/openvino/deployment_tools/model_optimizer/mo_tf.py"
 model="../model_pb/lenet.pd"
 
-${mo} --input_model ${model} --data_type FP32 --disable_nhwc_to_nchw --input_shape "(1,28,28,1)"
+#--disable_nhwc_to_nchw
+${mo} --input_model ${model} --data_type FP32 --input_shape [1,28,28,1]
 
 
